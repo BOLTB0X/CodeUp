e9462250cc601eb4142eefa853986ecbb4643eba
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-05-30 12:30:43 +0900

    C/051번~075번

---
+++ CodeUP_1071.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main() {
+	
+	int c;
+	    reload:
+
+	scanf("%d",&c);
+	
+	if (c != 0)
+	{
+		printf("%d\n", c);
+		goto reload;
+	}
+
+}
---
+++ CodeUP_1074.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main() {
+	
+	int n;
+
+	scanf("%d", &n);
+	while (n != 0)
+	{
+		printf("%d\n", n);
+		n--;
+	}	
+}
+
